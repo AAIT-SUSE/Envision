fe9136954de01ab88d195c12f0110b1c819a8715
Owen Tsai <owentsai.v@gmail.com>
2019-03-16 19:37:33 +0800

    Update editor config and dev server port
    - change dev server port to 8000 to match python server port

--- envision-frontend/src/plugins/editor.js
+++ envision-frontend/src/plugins/editor.js
@@ -3,4 +3,18 @@ import envisionEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import '../assets/envision-editor.css'
 
-vue.use(envisionEditor);
+vue.use(envisionEditor, {
+  modules: {
+		toolbar: [
+			['bold', 'italic', 'underline', 'strike'],
+			['blockquote', 'code-block'],
+			[{ 'list': 'ordered' }, { 'list': 'bullet' }],
+			[{ 'indent': '-1' }, { 'indent': '+1' }],
+			[{ 'header': [1, 2, 3, 4, false] }],
+			[{ 'font': ['默认', 'monospace'] }],
+			[{ 'color': [] }],
+			[{ 'align': [] }],
+			['link', 'image']
+		],
+	}
+});
